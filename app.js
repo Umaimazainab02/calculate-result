602d10df201e14e765a8b0d7c5427d03f69aff0f
Umaimazainab02 <umaimazainab429@gmail.com>
2026-07-10 12:28:09 +0500

    Added loop practice questions

---
+++ app.js
@@ -0,0 +1,9 @@
+//  calculate = document.getElementById("calculate");
+
+// calculate.addEventListener("click", function () {
+//     let english = Number(document.getElementById("english").value);
+//     let math = Number(document.getElementById("math").value);
+//     let computer =Number(document.getElementById("computer").value);
+//     let total = english + math + computer;
+//     document.getElementById("result").innerHTML = "Total Marks: " + total;
+// })
