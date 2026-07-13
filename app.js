46f6226d8a635485f40c10bd1db6f2c9ce88dd57
Umaimazainab02 <umaimazainab429@gmail.com>
2026-07-13 23:29:51 +0500

    Added loop practice questions

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-//  calculate = document.getElementById("calculate");
+let btn = document.getElementById("calculate");
 
-// calculate.addEventListener("click", function () {
-//     let english = Number(document.getElementById("english").value);
-//     let math = Number(document.getElementById("math").value);
-//     let computer =Number(document.getElementById("computer").value);
-//     let total = english + math + computer;
-//     document.getElementById("result").innerHTML = "Total Marks: " + total;
-// })
+btn.addEventListener("click", function () {
+
+    let english = Number(document.getElementById("english").value);
+    let math = Number(document.getElementById("math").value);
+    let computer = Number(document.getElementById("computer").value);
+
+    let total = english + math + computer;
+    let average = total / 3;
+
+    document.getElementById("total").innerHTML = "Obtain marks: " + total;
+
+    document.getElementById("average").innerHTML = "Average: " + average.toFixed(2);
+
+    if (average >= 40) {
+        document.getElementById("status").innerHTML = "Pass";
+    } else {
+        document.getElementById("status").innerHTML = "Fail";
+    }
+
+});
